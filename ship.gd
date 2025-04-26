8012396588a15e8bd21a1c9d909314e385c4b82c
Malthusis <58402624+Malthusis@users.noreply.github.com>
2025-04-26 16:53:04 -0600

    Add basic dive and rising animations for the ship.

--- ship.gd
+++ ship.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 var SPEED = 50
 var maxFuel = 1000
 var currentFuel = maxFuel
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
 
 ## On physics tick...
 func _physics_process(delta: float):
@@ -19,11 +20,15 @@ func _physics_process(delta: float):
 		if currentFuel >= 0:
 			currentFuel -= 1
 			velocity.y = -1 * SPEED
+			sprite.frame = 2
+			
 	elif Input.is_action_pressed("ui_down"):
 		velocity.y = SPEED
+		sprite.frame = 1
 	else:
 		#slowly start dropping (glide)
 		velocity.y = 6
+		sprite.frame = 0
 	
 	if currentFuel==0:
 		Events.out_of_fuel.emit()
