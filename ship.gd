aa5959062e54706bd1f097411a9af7664d800373
Cuba Giesbrecht <banishlight@gmail.com>
2025-04-26 14:17:54 -0600

    Ship fuel check added

--- ship.gd
+++ ship.gd
@@ -4,18 +4,24 @@ extends CharacterBody2D
 ##func _ready():
 
 var SPEED = 50
+var maxFuel = 100
+var currentFuel = maxFuel
 
 ## On physics tick...
 func _physics_process(delta: float):
 	if Input.is_action_pressed("ui_right"):
-		velocity.x = SPEED
+		if currentFuel >= 0:
+			currentFuel -= 1
+			velocity.x = SPEED
 	elif Input.is_action_pressed("ui_left"):
 		velocity.x = -1 * SPEED
 	else:
 		velocity.x = 0
 		
 	if Input.is_action_pressed("ui_up"):
-		velocity.y = -1 * SPEED
+		if currentFuel >= 0:
+			currentFuel -= 1
+			velocity.y = -1 * SPEED
 	elif Input.is_action_pressed("ui_down"):
 		velocity.y = SPEED
 	else:
